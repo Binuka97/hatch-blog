9b0a7209f08403705dd9056c7193a5fa27fe3911
Binuka Kamesh <33131182+Binuka97@users.noreply.github.com>
2020-08-05 16:55:40 +0530

    minor changes

--- src/PostDetailedView.js
+++ src/PostDetailedView.js
@@ -4,27 +4,28 @@ class PostDetailedView extends Component {
 
     state = {
         loading:true,
-        person:null
+        post:null
     };
 
     async componentDidMount(){
         const url = "https://jsonplaceholder.typicode.com/posts";
         const response = await fetch(url);
         const data = await response.json();
-        this.setState({person: data[0], loading:false})
+        this.setState({post: data[0], loading:false})
+        console.log(data);
         
     }
     render() {
         return(
             <div>
-                {this.state.loading || !this.state.person ? (
+                {this.state.loading || !this.state.post ? (
                     <div> Nothing to display </div>
                 ) : (
                 <div>
-                  <div> <h1>{this.state.person.title} </h1> </div>
+                  <div> <h1>{this.state.post.title} </h1> </div>
                   <br></br>
                   <br></br>
-                  <div> <p> {this.state.person.body} </p>  </div>
+                  <div> <p> {this.state.post.body} </p>  </div>
                 </div>
                 )}
             </div>
--- src/PostList.js
+++ src/PostList.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './PostList.css';
 import {Link} from 'react-router-dom'; 
+import PostDetailedView from './PostDetailedView'
 class PostList extends Component{
 
     state={
