c8c13c4bd53eccffbc166e146a6244a651e1c1ab
Ravindu-97 <61191868+Ravindu-97@users.noreply.github.com>
2020-08-05 15:34:57 +0530

    Added the api data

--- src/PostDetailedView.js
+++ src/PostDetailedView.js
@@ -1,11 +1,34 @@
 import React,{Component} from "react";
 
 class PostDetailedView extends Component {
+
+    state = {
+        loading:true,
+        person:null
+    };
+
+    async componentDidMount(){
+        const url = "https://jsonplaceholder.typicode.com/posts";
+        const response = await fetch(url);
+        const data = await response.json();
+        this.setState({person: data[0], loading:false})
+        
+    }
     render() {
         return(
-            <h1> Routing is working successfully!!!</h1>
-
-        )
+            <div>
+                {this.state.loading || !this.state.person ? (
+                    <div> Nothing to display </div>
+                ) : (
+                <div>
+                  <div> <h1>{this.state.person.title} </h1> </div>
+                  <br></br>
+                  <br></br>
+                  <div> <p> {this.state.person.body} </p>  </div>
+                </div>
+                )}
+            </div>
+       );
     }
 }
 
